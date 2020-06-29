9443a0c438fb12bb750ac985adface0b82d40a39
ET <Earnestwt3@gmail.com>
2020-06-29 16:12:22 -0500

    4 - Working with Static Members

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -3,31 +3,35 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
+      
+
     class Program
     {
+      
         static void Main(string[] args)
         {
-                      
+          
+       
+          
+            var book = new Book("Plus Ultra Grade Book");  
+            book.AddGrade(89.1);     
+            book.AddGrade(90.5); 
+            book.AddGrade(77.5);
+
             var grades = new List<double>(){12.7, 10.3, 6.11, 4.1};
             grades.Add(56.1);
             
             var result = 0.0;
+            var highGrade = double.MinValue;
             foreach (var number in grades)
-            {
+            {   
+                highGrade = Math.Max(number, highGrade);
                 result += number;
             }
             var average = result/grades.Count;
 
-            Console.WriteLine(result);
             Console.WriteLine($"The average grade is {average:N1}");
-            if(args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else
-            {
-                Console.WriteLine("Hello!");
-            }
+           
         }
 
     }
